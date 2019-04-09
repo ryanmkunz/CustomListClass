f3ab3398b421cf1ecc1d0223e9b5db87ac24c15f
Ryan Kunz <ryanmkunz@gmail.com>
2019-04-09 10:11:02 -0500

    Copied starter code from lecture

--- CustomListTesting/UnitTest1.cs
+++ CustomListTesting/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomListProject;
 
 namespace CustomListTesting
 {
@@ -9,6 +10,43 @@ namespace CustomListTesting
         [TestMethod]
         public void TestMethod1()
         {
+            //Arrange
+            CustomList<int> myList = new CustomList<int>();
+            int value = 5;
+
+            //Act
+            myList.Add(value);
+
+            //Assert
+            Assert.AreEqual(value, myList[0]);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //Arrange
+            CustomList<int> myList = new CustomList<int>();
+            int value = 5;
+            int expectedCount = 1;
+            //Act
+            myList.Add(value);
+
+            //Assert
+            Assert.AreEqual(expectedCount, myList.Count);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //Arrange
+            CustomList<int> myList = new CustomList<int>();
+            int value = 5;
+            int value1 = 10;
+
+            //Act
+            myList.Add(value);
+            myList.Add(value1);
+
+            //Assert
+            Assert.AreEqual(value, myList[0]);
         }
     }
 }
